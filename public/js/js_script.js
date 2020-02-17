7c244a2226a5ca54a1dd4b5437f702fa3a4042fa
prilven <philip.rilven.5557@student.uu.se>
2020-02-17 16:26:38 +0100

    Task 4-7

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,23 +1,26 @@
-function menuItem(name, kCal, image, allergy) {
+function menuItem(name, kCal, image, lactose, gluten, vegan) {
     this.name = name;
     this.kCal = kCal;
     this.image = image;
-    this.allergy = allergy;
+    this.lactose = lactose;
+    this.gluten = gluten;
+    this.vegan = vegan;
     this.info = function() {
 	return this.name + ' ' + this.kCal;
     };
 }
 
-let tastyBurger = new menuItem("Tasty Burger", 1200, "https://media-cdn.tripadcisor.com/media/photo-p/0f/a3/0e/a0/the-big-tasty.jpg", ("gluten", "lactose"));
+let tastyBurger = new menuItem("Tasty Burger", 1200, "https://media-cdn.tripadcisor.com/media/photo-p/0f/a3/0e/a0/the-big-tasty.jpg", 1, 1, 0);
 
-			       let cheeseBurger = new menuItem("Cheese Burger", 2400, "https://raster-static.postmates.com/?url=https%3A%2F%2Fd1725r39asqzt3.cloudfront.net%2F1dccdabc-7e12-4feb-aede-fe1407a73755%2Forig.jpg&quality=85&w=640&h=0&mode=auto&format=jpg&v=4", ("gluten","lactose"));
+let cheeseBurger = new menuItem("Cheese Burger", 2400, "https://raster-static.postmates.com/?url=https%3A%2F%2Fd1725r39asqzt3.cloudfront.net%2F1dccdabc-7e12-4feb-aede-fe1407a73755%2Forig.jpg&quality=85&w=640&h=0&mode=auto&format=jpg&v=4", 1, 1, 0);
 
-							       let avocadoBurger = new menuItem("Avocado Burger", 32, "https://2.bp.blogspot.com/-eka2VX3O75E/Whve_hSWOUI/AAAAAAABerc/OSVMFDaXq34BeNPewN4MkLXW33v3_2M0wCLcBGAs/s1600/avocado-burger%2Bfood%2B%2526%2Bdrink%2Btips%2BEN%2BBLANDAD%2BKOMPOTT.jpg", ("vegan"));
+let avocadoBurger = new menuItem("Avocado Burger", 32, "https://2.bp.blogspot.com/-eka2VX3O75E/Whve_hSWOUI/AAAAAAABerc/OSVMFDaXq34BeNPewN4MkLXW33v3_2M0wCLcBGAs/s1600/avocado-burger%2Bfood%2B%2526%2Bdrink%2Btips%2BEN%2BBLANDAD%2BKOMPOTT.jpg", 0, 0, 1);
 
-												let turkeyBurger = new menuItem("Turkey Burger", 650, "https://assets.epicurious.com/photos/5748b2ed5a5fbbae31ae4af9/master/pass/shutterstock_148839410.jpg", (""));
+let turkeyBurger = new menuItem("Turkey Burger", 650, "https://assets.epicurious.com/photos/5748b2ed5a5fbbae31ae4af9/master/pass/shutterstock_148839410.jpg", 0, 0, 0);
 
-												let halloumiBurger = new menuItem("Halloumi Burger", 200, "https://images.ctfassets.net/qu53tdnhexvd/2cSAd70I9aOKiYeCswqE0K/d4a6118cd7ac072a3a28756e1bac7d3c/chorizo-_-halloumi-burgers.jpg?fm=jpg&fl=progressive&q=80&w=1300", (""));
+let halloumiBurger = new menuItem("Halloumi Burger", 200, "https://images.ctfassets.net/qu53tdnhexvd/2cSAd70I9aOKiYeCswqE0K/d4a6118cd7ac072a3a28756e1bac7d3c/chorizo-_-halloumi-burgers.jpg?fm=jpg&fl=progressive&q=80&w=1300", 0, 0, 0);
 
+/*
 let list1 = document.createElement('ul');
 let listitem1 = document.createElement('li');
 let listitem2 = document.createElement('li');
@@ -43,3 +46,32 @@ list1.appendChild(listitem4);
 list1.appendChild(listitem5);
 
 document.body.appendChild(list1);
+*/
+
+let menu = new Array(tastyBurger, cheeseBurger, avocadoBurger, turkeyBurger, halloumiBurger);
+
+let myElement = document.getElementById("myList")
+for(let item of menu){
+	let listItem = document.createElement('li');
+	let listValue = document.createTextNode(item.info());
+  listItem.appendChild(listValue);
+  myElement.appendChild(listItem);
+  if(item.lactose){
+  	let listItem = document.createElement('p');
+    let listValue = document.createTextNode('Lactose');
+    listItem.appendChild(listValue);
+    myElement.appendChild(listItem);
+  }
+    if(item.gluten){
+  	let listItem = document.createElement('p');
+    let listValue = document.createTextNode('Gluten');
+    listItem.appendChild(listValue);
+    myElement.appendChild(listItem);
+    }
+      if(item.vegan){
+  	let listItem = document.createElement('p');
+    let listValue = document.createTextNode('Vegan');
+    listItem.appendChild(listValue);
+    myElement.appendChild(listItem);
+    }
+}
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,10 +1,28 @@
 const vm = new Vue( {
     el: '#myID',
     data: {
-	burgare1: tastyBurger.info(),
-	burgare2: cheeseBurger.info(),
-	burgare3: avocadoBurger.info(),
-	burgare4: turkeyBurger.info(),
-	burgare5: halloumiBurger.info()
+	menu: menu,
     }
 })
+
+
+<script src="https://vuejs.org/js/vue.js"></script>
+<div id="myID">
+  <h1>
+    Välj en burgare
+  </h1>
+  <ul>
+    <li v-for="item in menu">
+      {{item.info()}}
+      <p v-if="item.lactose">
+      Lactose
+      </p>
+      <p v-if="item.gluten">
+      Gluten
+      </p>
+      <p v-if="item.vegan">
+      Vegan
+      </p>
+    </li>
+  </ul>
+</div>
