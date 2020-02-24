b28bdf1c03699bf6142f0f69a2caa5d955b7e16b
prilven <philip.rilven.5557@student.uu.se>
2020-02-24 10:33:47 +0100

    Task 5-2

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,16 +1,25 @@
+'use strict';
+const socket = io();
 
-
-const vm1 = new Vue( {
+const vm = new Vue( {
     el: '#main',
     data: {
+	orders: {},
 	food: food,
 	order: '',
 	formName: '',
 	formEmail: '',
-	formStreet: '',
-	formHouse: '',
 	formPayment: 'Swish',
 	formGender: 'other',
+    },
+    created: function() {
+	socket.on('initialize', function(data) {
+	    this.orders = data.orders;
+	}.bind(this));
+
+	socket.on('currentQueue', function(data) {
+	    this.orders = data.orders;
+	}.bind(this));
     },
     methods: {
 	submitOrder: function () {
@@ -20,9 +29,29 @@ const vm1 = new Vue( {
 		    burgers.push(item.name);
 		}
 	    }
-	    this.order = new Array(burgers, this.formName, this.formEmail, this.formStreet, this.formHouse, this.formPayment, this.formGender);
-	}
-    }
+	    this.order = new Array(burgers, this.formName, this.formEmail, this.formPayment, this.formGender);
+	},
+	getNext: function() {
+	let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+	    return Math.max(last, next);
+	}, 0);
+	    return lastOrder + 1;
+	},
+	addOrder: function(event) {
+	    let offset = {
+		x: event.currentTarget.getBoundingClientRect().left,
+		y: event.currentTarget.getBoundingClientRect().top,
+	    };
+	    socket.emit('addOrder', {
+		orderId: this.getNext(),
+		details: {
+		    x: event.clientX - 10 - offset.x,
+		    y: event.clientY - 10 - offset.y,
+		},
+		orderItems: ['Beans', 'Curry'],
+	    });
+	},					
+    },
 } )
 
 
